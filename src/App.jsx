4eb332a49f302b8f3b759e1789c1557eb7408ffd
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-03-25 08:06:47 -0700

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,44 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+//Class Components
+//Not widely used now. 
+// class ClassComponent extends React.Component {
+//   render(){
+//     return (
+//       <div className="card">
+//         <h2>Class Component</h2>
+//         <p>Hello, I am a class component.</p>
+//       </div>
+//     )
+//   }
+// }
 
+//Components are reuseable. lets create a new Component card component and reused it in the app compoenent which is the master component
+function Card() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+      <h2>My Card component</h2>
+     
+    </div>
   )
+}
+
+
+
+//Function Component
+function App() {
+return (
+  <div>
+  <h2>Functional Component</h2>
+  <Card/>
+  <Card/>
+  <Card/>
+  <Card/>
+  </div>
+
+)
+  
 }
 
 export default App
