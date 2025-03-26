352e83d6486a5016b8f684e2b18b8ddc22e68d86
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-03-26 09:47:43 -0700

    Setting our hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import hero from './assets/hero.png'
 
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline blue">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern"/>
+      <div className="wrapper">
+        <header>
+          <img src={hero} alt="Hero Banner"/>
+          <h1> Find <span class="text-gradient">Movies </span>You Will Enjoy Without A Hassle</h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
   );
 }
 
