c694a4888f91b07b8755ae0b4b841bb921d56fbb
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-03-27 07:44:58 -0700

    Displaying the movies with all the datas in the ui for the users to see. and adding of spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import hero from './assets/hero.png'
 import Search from "./Components/Search";
+import Spinner from "./Components/Spinner";
 
 //API - Application Programming Interface - a set of rules that allows onesoftware application to talk to another application.
 
@@ -25,11 +26,23 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('')
   //error message state
   const [errorMessage, setErrorMessage] = useState('')
+  //movies state
+  const [moviesList, setMoviesList] = useState([])
+  //loading state
+  const [isLoading, setIsLoading] = useState(false)
+
 
   //Declare a function that will allow you to fetch those movies.
 
   const fetchMovies = async () => {
     try {
+      //show loading state
+      setIsLoading(true)
+      //clear the error message
+      setErrorMessage('')
+
+      
+      //We're using the TMDB API to fetch movies in descending order by popularity.
       //define exact endpoint you are trying to call
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
       //Then call the endpoint
@@ -40,11 +53,23 @@ function App() {
       }
       //If successful, parse the response as JSON and log the data to the console.
       const data = await response.json()
-      console.log(data)
+      //Set the movies state with the received data
+      if(data.response === 'false'){
+        setErrorMessage(data.Error || 'failed to fetch movies');
+        setMoviesList([])
+        setIsLoading(false)
+        return
+      }
+      //if fetching is successful
+      setMoviesList(data.results || []);
+      
       //Set the movies state with the received data
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage(`Error fetching movies: ${error.message}`);
+    }
+    finally{
+      setIsLoading(false)
     }
   }
 
@@ -63,8 +88,29 @@ function App() {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
        <section className="all-movies">
-           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-          <h2>Search Results:</h2>
+          <h2  className="mt-40">All Movies</h2>
+         {/* Advance conditional rendeering */}
+         {isLoading ? (
+          <Spinner/>
+         ): errorMessage ? (
+          <p className="text-red-500">
+            {errorMessage}
+          </p>
+         ): (
+          <ul>
+            {moviesList.map((movie) => (
+              <li key={movie.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                <h3 className="text-white">{movie.title}</h3>
+                <p className="text-white">{movie.overview.substring(0, 150)}...</p>
+              </li>
+            ))}
+          </ul>
+         )}
+         
        </section>
       </div>
     </main>
